17771a37de13d4b52df7131b69913ba47ec6564c
Jerry Dudum <jsdudum@gmail.com>
2019-10-23 19:27:41 -0700

    able to scrape articles from pcgamer website and they show up in /articles now just need to append to the page with handlebars and add some styling and client side javascript

--- server.js
+++ server.js
@@ -24,6 +24,97 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+app.get("/", function (req, res) {
+    db.Article.find({}).then(function (response) {
+        var dbResponse = {
+            articles: response
+        };
+        res.render("index", dbResponse);
+    })
+        .catch(function (err) {
+            console.log(err);
+            res.send(err);
+        });
+});
+
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.pcgamer.com/news/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("div.content").each(function (i, element) {
+            var result = {};
+
+            result.title = $(element)
+                .children("header")
+                .children("h3")
+                .text();
+
+            result.summary = $(element)
+                .children("p")
+                .text()
+
+            result.image = $(element)
+                .parent()
+                .children("div.image")
+                .children()
+                .data("original");
+
+            result.link = $(element)
+                .parent()
+                .parent()
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.send(dbArticle);
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
+});
+
+app.get("/articles/:id", function (req, res) {
+
+    var ID = req.params.id;
+
+    db.Article.findOne({ _id: ID })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
+});
+
+app.post("/articles/:id", function (req, res) {
+
+    var ID = req.params.id;
+
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: ID }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
