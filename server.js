4bcdbb51168836047cb7d6d501a65aa30f423ea6
Jerry Dudum <jsdudum@gmail.com>
2019-10-23 18:25:07 -0700

    added handlebars view folder and got the basic layout of the server done

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 var express = require("express");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
 
-
-
-
-
-
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 app.listen(PORT, function () {
